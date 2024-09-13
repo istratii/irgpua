f4467fd497deb41c703d36612eb021f0807e31c1
istratii <istratii@users.noreply.github.com>
2024-09-13 11:00:16 +0000

    Commit linter changes

--- src/pipeline.hh
+++ src/pipeline.hh
@@ -2,43 +2,40 @@
 
 #include "image.hh"
 
-#include <string> 
 #include <memory>
-#include <stack>
 #include <regex>
+#include <stack>
+#include <string>
 
 static std::string get_number(const std::string& str)
 {
-    std::regex r("Broken#(\\d+)");
-    std::smatch match;
+  std::regex r("Broken#(\\d+)");
+  std::smatch match;
 
-    if (std::regex_search(str, match, r) && match.size() > 1)
-        return match.str(1);
-    else
+  if (std::regex_search(str, match, r) && match.size() > 1)
+    return match.str(1);
+  else
     {
-        std::cerr << "Error file name" << std::endl;
-        exit(-1);
-        return "Error file name";
+      std::cerr << "Error file name" << std::endl;
+      exit(-1);
+      return "Error file name";
     }
 }
 
 struct Pipeline
 {
-    Pipeline(const std::vector<std::string>& filepaths)
-    {
-        images = std::vector<Image>(filepaths.size());
-        #pragma omp parallel for
-        for (std::size_t i = 0; i < filepaths.size(); ++i)
-        {
-            const int image_id = std::stoi(get_number(filepaths[i]));
-            images[i] = Image(filepaths[i], image_id);
-        }
-    }
+  Pipeline(const std::vector<std::string>& filepaths)
+  {
+    images = std::vector<Image>(filepaths.size());
+#pragma omp parallel for
+    for (std::size_t i = 0; i < filepaths.size(); ++i)
+      {
+        const int image_id = std::stoi(get_number(filepaths[i]));
+        images[i] = Image(filepaths[i], image_id);
+      }
+  }
 
-    Image&& get_image(int i)
-    {
-        return std::move(images[i]);
-    }
+  Image&& get_image(int i) { return std::move(images[i]); }
 
-    std::vector<Image> images;
+  std::vector<Image> images;
 };
