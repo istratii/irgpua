f8c943d1117de38355d02aabe51112f321eb00d4
etienne2509 <etienne2509@users.noreply.github.com>
2024-09-17 19:51:34 +0000

    Commit linter changes

--- src/apply_map_fix.cu
+++ src/apply_map_fix.cu
@@ -1,10 +1,10 @@
 __global__ void map_kernel(int* d_buffer, int size)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < size)
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < size)
     {
-        // on evite le plus possible le branching avec trop de if
-        const int assoc_val[4] = {1, -5, 3, -8};
-        d_buffer[idx] += ops[idx % 4];
+      // on evite le plus possible le branching avec trop de if
+      const int assoc_val[4] = {1, -5, 3, -8};
+      d_buffer[idx] += ops[idx % 4];
     }
 }
