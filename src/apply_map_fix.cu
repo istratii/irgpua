d77ef3a5102bbd78a758d17d7d6a0ea63f107aa6
Etienne Reverechon <etienne.reverchon@epita.fr>
2024-09-23 11:19:44 +0200

    feat(fix_map): add macro for nicolae

--- src/apply_map_fix.cu
+++ src/apply_map_fix.cu
@@ -1,10 +1,10 @@
+#define ASSOC_VAL(idx) (((idx) % 4 == 0) ? 1 : \
+                        ((idx) % 4 == 1) ? -5 : \
+                        ((idx) % 4 == 2) ? 3 : -8)
+
 __global__ void map_kernel(int* d_buffer, int size)
 {
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < size)
-    {
-      // on evite le plus possible le branching avec trop de if
-      const int assoc_val[4] = {1, -5, 3, -8};
-      d_buffer[idx] += ops[idx % 4];
-    }
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size)
+      d_buffer[idx] += ASSOC_VAL(idx);
 }
