e6ab6374b709084182d5a292056465d6efb45cb4
etienne2509 <etienne2509@users.noreply.github.com>
2024-09-23 09:20:08 +0000

    Commit linter changes

--- src/apply_map_fix.cu
+++ src/apply_map_fix.cu
@@ -1,10 +1,9 @@
-#define ASSOC_VAL(idx) (((idx) % 4 == 0) ? 1 : \
-                        ((idx) % 4 == 1) ? -5 : \
-                        ((idx) % 4 == 2) ? 3 : -8)
+#define ASSOC_VAL(idx)                                                         \
+  (((idx) % 4 == 0) ? 1 : ((idx) % 4 == 1) ? -5 : ((idx) % 4 == 2) ? 3 : -8)
 
 __global__ void map_kernel(int* d_buffer, int size)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < size)
-      d_buffer[idx] += ASSOC_VAL(idx);
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < size)
+    d_buffer[idx] += ASSOC_VAL(idx);
 }
