5a0d322cdca0bcfa78c00ea92012bb48e37aea83
nicolae.istratii <nicolae.istratii@epita.fr>
2024-09-24 23:41:31 +0200

    update: fix_gpu

--- src/fix_gpu.cu
+++ src/fix_gpu.cu
@@ -4,13 +4,12 @@ void fix_image_gpu(Image& to_fix)
 {
   const unsigned int true_size = to_fix.size();
   const unsigned int image_size = to_fix.width * to_fix.height;
-
   const raft::handle_t handle{};
   cudaStream_t stream = handle.get_stream();
 
   rmm::device_uvector<int> buffer(true_size, stream);
-  CUDA_CHECK_ERROR(cudaMemcpy(buffer.data(), to_fix.buffer,
-                              true_size * sizeof(int), cudaMemcpyHostToDevice));
+  CUDA_CHECK_ERROR(cudaMemcpyAsync(buffer.data(), to_fix.buffer, true_size * sizeof(int),
+                                   cudaMemcpyHostToDevice, stream));
 
   // #1 Compact
   compact(buffer);
@@ -19,11 +18,10 @@ void fix_image_gpu(Image& to_fix)
   // #2 Apply map to fix pixels
   map_fix(buffer);
 
-  // #3 Histogram equalization
+  // // #3 Histogram equalization
   rmm::device_uvector<int> hist = histogram(buffer);
   equalize_histogram(buffer, hist);
 
-  CUDA_CHECK_ERROR(cudaMemcpy(to_fix.buffer, buffer.data(),
-                              image_size * sizeof(int),
-                              cudaMemcpyDeviceToHost));
+  CUDA_CHECK_ERROR(cudaMemcpyAsync(to_fix.buffer, buffer.data(), image_size * sizeof(int),
+                                   cudaMemcpyDeviceToHost, stream));
 }
