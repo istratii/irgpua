107d901a8a62ead29bcdb7847b83c8d080cf264c
nicolae.istratii <nicolae.istratii@epita.fr>
2024-10-09 22:09:32 +0200

    feature: added init for memory pool

---
+++ src/cuda_tools/memory_pool.cu
@@ -0,0 +1,14 @@
+
+#include "memory_pool.cuh"
+
+#define ALIGN32(N) ((N + 31) & ~31)
+
+void init_memory_pool(size_t bytes)
+{
+  bytes = ALIGN32(bytes);
+  auto cuda_memory_resource = std::make_shared<rmm::mr::cuda_memory_resource>();
+  auto pool_ressource = std::make_shared<
+    rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>(
+    cuda_memory_resource.get(), bytes);
+  rmm::mr::set_current_device_resource(pool_ressource.get());
+}
