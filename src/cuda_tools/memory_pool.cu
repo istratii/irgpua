03ac90be499fd33258af34e0db0ebf0587586ac4
nicolae.istratii <nicolae.istratii@epita.fr>
2024-10-09 22:21:29 +0200

    fix: use static globals

--- src/cuda_tools/memory_pool.cu
+++ src/cuda_tools/memory_pool.cu
@@ -1,12 +1,22 @@
 
 #include "memory_pool.cuh"
 
+static std::shared_ptr<rmm::mr::cuda_memory_resource> cuda_memory_resource;
+static std::shared_ptr<
+  rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>
+  pool_resource;
+
 void init_memory_pool(size_t bytes)
 {
   bytes = ALIGN32(bytes);
-  auto cuda_memory_resource = std::make_shared<rmm::mr::cuda_memory_resource>();
-  auto pool_ressource = std::make_shared<
-    rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>(
-    cuda_memory_resource.get(), bytes);
-  rmm::mr::set_current_device_resource(pool_ressource.get());
+
+  if (!cuda_memory_resource)
+    cuda_memory_resource = std::make_shared<rmm::mr::cuda_memory_resource>();
+
+  if (!pool_resource)
+    pool_resource = std::make_shared<
+      rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>(
+      cuda_memory_resource.get(), bytes);
+
+  rmm::mr::set_current_device_resource(pool_resource.get());
 }
