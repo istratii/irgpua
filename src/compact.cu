36cbed6f7a034fc6f057f014ae4a0225a850bc78
nicolae.istratii <nicolae.istratii@epita.fr>
2024-11-03 14:50:53 +0100

    some optimizations (no)

--- src/compact.cu
+++ src/compact.cu
@@ -2,6 +2,7 @@
 
 #define GARBAGE_VAL -27
 
+#ifdef _IRGPUA_GPU
 static __global__ void _compact(raft::device_span<int> buffer_dspan,
                                 raft::device_span<int> pred_dspan)
 {
@@ -9,6 +10,23 @@ static __global__ void _compact(raft::device_span<int> buffer_dspan,
   if (idx < buffer_dspan.size())
     pred_dspan[idx] = buffer_dspan[idx] != GARBAGE_VAL;
 }
+#else // _IRGPUA_GPU_INDUS
+struct IsNotGarbage
+{
+  __device__ bool operator()(const int x) const { return x != GARBAGE_VAL; }
+};
+
+// struct ScatterUnaryFunction
+// {
+//   raft::device_span<int> buffer_dspan;
+//   raft::device_span<int> pred_dspan;
+//   __device__ void operator()(int ii) const
+//   {
+//     if (buffer_dspan[ii] != GARBAGE_VAL)
+//       buffer_dspan[pred_dspan[ii]] = buffer_dspan[ii];
+//   }
+// };
+#endif
 
 static __global__ void _scatter(raft::device_span<int> buffer_dspan,
                                 raft::device_span<int> pred_dspan)
@@ -17,13 +35,6 @@ static __global__ void _scatter(raft::device_span<int> buffer_dspan,
   if (idx < buffer_dspan.size() && buffer_dspan[idx] != GARBAGE_VAL)
     buffer_dspan[pred_dspan[idx]] = buffer_dspan[idx];
 }
-#ifdef _IRGPUA_GPU
-#else // _IRGPUA_GPU_INDUS
-struct IsNotGarbage
-{
-  __device__ bool operator()(const int x) const { return x != GARBAGE_VAL; }
-};
-#endif
 
 void compact(rmm::device_buffer& memchunk, raft::device_span<int> buffer_dspan)
 {
@@ -46,6 +57,16 @@ void compact(rmm::device_buffer& memchunk, raft::device_span<int> buffer_dspan)
                     buffer_dspan.end(), pred_dspan.begin(), IsNotGarbage());
   thrust::exclusive_scan(thrust::cuda::par.on(stream), pred_dspan.begin(),
                          pred_dspan.end(), pred_dspan.begin());
+  // just doesn't work, idk
+  // thrust::scatter_if(thrust::cuda::par.on(stream), buffer_dspan.begin(),
+  //                    buffer_dspan.end(), pred_dspan.begin(),
+  //                    buffer_dspan.begin(), buffer_dspan.begin(),
+  //                    IsNotGarbage());
+  // this also appears to now work
+  // thrust::for_each(thrust::cuda::par.on(stream),
+  //                  thrust::make_counting_iterator(0UL),
+  //                  thrust::make_counting_iterator(buffer_dspan.size()),
+  //                  ScatterUnaryFunction{buffer_dspan, pred_dspan});
 #endif
   _scatter<<<grid_size, block_size, 0, stream>>>(buffer_dspan, pred_dspan);
 }
--- src/cuda_tools/constants.hh
+++ src/cuda_tools/constants.hh
@@ -6,15 +6,36 @@
 constexpr size_t max_image_size = ALIGN128(4'016'251);
 constexpr size_t bytes_per_image = max_image_size * sizeof(int);
 constexpr size_t bytes_per_predicate = bytes_per_image;
+#ifdef _IRGPUA_GPU
 constexpr size_t bytes_per_scan = ALIGN128(sizeof(int) * (4096 * 3 + 1));
+#else // _IRGPUA_GPU_INDUS
+constexpr size_t bytes_per_temp_storage = ALIGN128(142'000);
+#endif
 constexpr size_t bytes_per_histogram = sizeof(int) * 256;
 constexpr size_t bytes_per_cdf_min = ALIGN128(sizeof(int));
 constexpr size_t bytes_per_total = ALIGN128(sizeof(int));
 constexpr size_t bytes_per_chunk = bytes_per_image + bytes_per_predicate
-  + bytes_per_scan + bytes_per_histogram + bytes_per_cdf_min + bytes_per_total;
+  + bytes_per_histogram + bytes_per_cdf_min + bytes_per_total +
+#ifdef _IRGPUA_GPU
+  bytes_per_scan
+#else // _IRGPUA_GPU_INDUS
+  bytes_per_temp_storage
+#endif
+  ;
 constexpr size_t buffer_offset = 0;
 constexpr size_t predicate_offset = buffer_offset + bytes_per_image;
-constexpr size_t scan_offset = predicate_offset + bytes_per_predicate;
-constexpr size_t histogram_offset = scan_offset + bytes_per_scan;
+#ifdef _IRGPUA_GPU
+constexpr size_t scan_offset =
+#else // _IRGPUA_GPU_INDUS
+constexpr size_t temp_storage_offset =
+#endif
+  predicate_offset + bytes_per_predicate;
+constexpr size_t histogram_offset =
+#ifdef _IRGPUA_GPU
+  scan_offset + bytes_per_scan
+#else // _IRGPUA_GPU_INDUS
+  temp_storage_offset + bytes_per_temp_storage
+#endif
+  ;
 constexpr size_t cdf_min_offset = histogram_offset + bytes_per_histogram;
 constexpr size_t total_offset = cdf_min_offset + bytes_per_cdf_min;
